e67986c3a0b4f3978f8f8da0db054c1b6d6d5915
Aayush Jaiswal <aay7ush@gmail.com>
2023-10-04 17:55:31 +0530

    slight ui change

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,16 +14,16 @@ const Hero = () => {
     <div className="flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto">
       <div className="flex-1 pt-36 sm:px-16 px-6">
         <h1 className="2xl:text-[72px] sm:text-[64px] text-[50px] font-extrabold">
-          Find, book, or rent a car - quickly and easily!
+          Discover Your Dream Car - quickly and easily!
         </h1>
 
         <p className="text-[27px] text-black-100 font-light mt-5">
-          Streamline your car rental experience with our effortless booking
-          process.
+          Find the perfect car for your needs with our comprehensive car search
+          and filters.
         </p>
 
         <CustomButton
-          title="Explore Cars"
+          title="Search Now"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
